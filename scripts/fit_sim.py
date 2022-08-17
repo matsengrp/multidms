e04474b8477078c9a9a3fe42ef5d0e8898664b7f
jgallowa07 <jaredgalloway07@gmail.com>
2022-08-17 07:20:04 -0700

    cleanup

--- scripts/fit_sim.py
+++ scripts/fit_sim.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
 
+# python native
+import pickle
+from timeit import default_timer as timer
+import os
+
 # dependencies
 import pandas as pd
 import numpy as np
@@ -13,13 +18,11 @@ jax.config.update("jax_enable_x64", True)
 import jax.numpy as jnp
 from jax.experimental import sparse
 import jaxopt
+
 from jaxopt import ProximalGradient
 
 # local
 import sys
-import pickle
-from timeit import default_timer as timer
-import os
 sys.path.append("../")
 from multidms.utils import *
 from multidms.model import *
@@ -27,13 +30,6 @@ from multidms.model import *
 simulated_dataset = pd.read_csv("../results/simulated_dataset_v1.csv")
 simulated_dataset.aa_substitutions.fillna("", inplace=True)
 simulated_dataset_lib1 = simulated_dataset.query("library == 'lib_1'").copy()
-simulated_dataset_lib1 = simulated_dataset_lib1.sample(n=35000, random_state=23)
-
-#simulated_dataset_lib1_ref = simulated_dataset_lib1.query("homolog == 'reference'").copy()
-#print(len(simulated_dataset_lib1))
-
-
-#simulated_mut_effects.replace({"":"", "":""}, axis=1)
 
 homologs = json.load(open("../results/homolog_aa_seqs.json", "r"))
 homologs["reference"] = homologs['1']
@@ -70,55 +66,30 @@ print(f"\nPre-Optimization")
 print(f"----------------")
 print(f"cost = {cost_smooth(params, (X, y)):.2e}")
 
-#pg = ProjectedGradient(fun=fun, projection=projection_non_negative)
-#pg_sol = pg.run(w_init, data=(X, y)).params
-
 tol = 1e-6
 maxiter = 10000
-#latent_solver = jaxopt.GradientDescent(cost_smooth_latent, tol=tol, maxiter=maxiter)
-##solver = jaxopt.ProjectedGradient(cost_smooth_latent, tol=tol, maxiter=maxiter)
-#
-#start = timer()
-##h2_params = params["S_H2"].copy()
-##beta_params = params["β"].copy()
-#
-#params, state = latent_solver.run(
-#    params, 
-#    data=(
-#        {"reference" : X["reference"]}, 
-#        {"reference" : y["reference"]}
-#    )
-#)
-#
-## make sure only the beta params were tuned.
-##assert jnp.all(params["S_H2"] == h2_params)
-##assert not jnp.all(params["β"] == beta_params)
-#
-#params, state = latent_solver.run(params, data=(X, y))
-#
-#solver = ProximalGradient(cost_smooth, prox, tol=tol, maxiter=maxiter)
-#params, state = solver.run(
-#    params, 
-#    hyperparams_prox = dict(
-#        clip_stretch=0.0, 
-#        #lock_params={"β":params["β"]}
-#        lock_params={"β":params["β"], "S_H2":params["S_H2"]}
-#    ),
-#    data=(X, y)
-#)
-#params, state = solver.run(
-#    params, 
-#    hyperparams_prox = dict(
-#        clip_stretch=0.0, 
-#        lock_params=None
-#        #lock_params={"β":params["β"]}
-#        #lock_params={"β":params["β"], "S_H2":params["S_H2"]}
-#    ),
-#    data=(X, y)
-#)
-#end = timer()
-
 start = timer()
+solver_latent = ProximalGradient(cost_smooth_latent, prox, tol=tol, maxiter=maxiter)
+
+h2_params = params["S_H2"].copy()
+beta_params = params["β"].copy()
+
+params, state = solver_latent.run(
+    params, 
+    hyperparams_prox = dict(
+        clip_stretch=0.0, 
+        lock_params=None
+    ),
+    data=(
+        {"reference" : X["reference"]}, 
+        {"reference" : y["reference"]}
+    )
+)
+
+# make sure only the beta params were tuned.
+assert jnp.all(params["S_H2"] == h2_params)
+assert not jnp.all(params["β"] == beta_params)
+
 solver = ProximalGradient(cost_smooth, prox, tol=tol, maxiter=maxiter)
 params, state = solver.run(
     params, 
@@ -165,8 +136,6 @@ print(f"\nFit Sigmoid Parameters, α\n================")
 for param, value in params['α'].items():
     print(f"{param}: {value}") 
 
-
-
 df["latent_predicted"] = onp.nan
 df["observed_predicted"] = onp.nan
 
@@ -184,7 +153,4 @@ print(f"Done")
 
 simulated_mut_effects = pd.read_csv("../results/simulated_mut_effects_v1.csv")
 results = (params, (X, y), df, simulated_mut_effects, all_subs, homologs)
-pickle.dump(results, open("../_ignore/simulated_results_test_single.pkl", "wb"))
-
-
-
+pickle.dump(results, open("../_ignore/simulated_results_test_double.pkl", "wb"))
