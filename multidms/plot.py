2624d623cc82f2f5cd5370314e9c670140b73cb9
jgallowa07 <jaredgalloway07@gmail.com>
2022-10-13 16:36:06 -0700

    updated pipeline notebook to be able to run any number of conditions

--- multidms/plot.py
+++ multidms/plot.py
@@ -35,52 +35,44 @@ def plot_pred_scatter(
             return True if len(string.split()) == 0 else False
         
         df = df.assign(is_wt = df[substitution_column].apply(is_wt))
-        fig, ax = plt.subplots(1, 2, figsize=[10, 4], sharey=False)
-
+        fig, ax = plt.subplots(1, 2, figsize=[10, 4], sharey=True)
 
         sns.scatterplot(
-            data=df, x=f"predicted_{row.func_score_target}",
-            y=row.func_score_target,
+            data=df, x=f"predicted_func_score",
+            y=f"corrected_func_score",
             hue=experiment_column if hue else None,
             alpha=0.01, palette="deep", ax=ax[0],
             legend=False
         )
-        for group, wt_exp_df in df.query(f"is_wt == True").groupby([experiment_column]):
-            wt_pred = wt_exp_df[f"predicted_{row.func_score_target}"].apply(lambda x: round(x, 5)).unique()
-            assert len(wt_pred) == 1
-            ls = "-" if group == row.experiment_ref else "--"
-            co = "blue" if group == row.experiment_ref else "orange"
-            ax[0].axvline(wt_pred[0], color=co, ls=ls, label=group)
 
-        lb = df[[row.func_score_target, f"predicted_{row.func_score_target}"]].min().min()
-        ub = df[[row.func_score_target, f"predicted_{row.func_score_target}"]].max().max()
+        for group, wt_exp_df in df.query(f"is_wt == True").groupby([experiment_column]):
+            wt_pred = wt_exp_df[f"predicted_func_score"].apply(lambda x: round(x, 5)).unique()
+            assert len(wt_pred) == 1
+            ax[0].axvline(wt_pred[0], label=group)
+
+        lb = df[['func_score', f"predicted_func_score"]].min().min()
+        ub = df[['func_score', f"predicted_func_score"]].max().max()
 
         ax[0].plot([lb, ub], [lb, ub], "k--", lw=1)
-        r = pearsonr(df[row.func_score_target], df[f'predicted_{row.func_score_target}'])[0]
+        r = pearsonr(df[f'corrected_func_score'], df[f'predicted_func_score'])[0]
         ax[0].annotate(f"$r = {r:.2f}$", (.1, .9), xycoords="axes fraction", fontsize=12)
         ax[0].set_ylabel("functional score")
         ax[0].set_xlabel("predicted functional score")
 
-
-        idx = df.query(f"{experiment_column} == '{row.experiment_2}'").index 
-        df_ep = df.copy()
         if row.model == "non-linear":
-            df_ep.loc[idx, row.func_score_target] -= row.tuned_model_params[f"γ_{row.experiment_2}"][0]
             sns.scatterplot(
-                data=df_ep, x="predicted_latent_phenotype",
-                y=row.func_score_target,
-                # hue=experiment_column,
+                data=df, x="predicted_latent_phenotype",
+                y=f"corrected_func_score",
                 hue=experiment_column if hue else None,
                 alpha=0.01, palette="deep",
                 legend=True, ax=ax[1]
             )
+
         if row.model == "non-linear":
             for group, wt_exp_df in df.query(f"is_wt == True").groupby([experiment_column]):
                 wt_pred = wt_exp_df[f"predicted_latent_phenotype"].apply(lambda x: round(x, 5)).unique()
                 assert len(wt_pred) == 1
-                ls = "-" if group == row.experiment_ref else "--"
-                co = "blue" if group == row.experiment_ref else "orange"
-                ax[1].axvline(wt_pred[0], color=co, ls=ls, label=f"{group} wt")
+                ax[1].axvline(wt_pred[0], label=group)
 
         if row.model == "non-linear":
             ax[1].legend(loc= "center right", bbox_to_anchor=(1.7, 1.00))
@@ -90,6 +82,7 @@ def plot_pred_scatter(
             1.1 * df.predicted_latent_phenotype.max(),
             num=1000
         )
+
         if "α" in row.tuned_model_params:
             shape = (ϕ_grid, g(row.tuned_model_params["α"], ϕ_grid))
             ax[1].plot(*shape, color='k', lw=1)
@@ -118,21 +111,22 @@ def plot_pred_scatter(
                 size=8)
 
         ax[1].set_xlabel("predicted_latent_phenotype (ϕ)")
-        ax[1].set_ylabel("functional score - γ")
+        ax[0].set_ylabel("functional score - γ$_{h}$")
         # ax[1].plot(*shape, color='k', lw=1)
-        # ax[1].set_ylim(-4, 2.5)       
-        #ax[0].set_xlim(-5, 2.5)       
-        #ax[0].set_ylim(-5, 3)       
+        # ax[0].set_ylim(-5, 2.5)       
         ax[1].set_xlim(-11, 6)       
-        #ax[1].set_ylim(-11, 6)       
-        #ax[0].set_xlim(-11, 6)       
-        #ax[0].set_ylim(-11, 6)       
-        #ax[1].set_ylim(-5, 3)       
         plt.tight_layout()
+        ax[0].grid()
+        ax[1].grid()
         if save:
             saveas = "scatter"
             for key, value in row.items():
-                if key in ["tuned_model_params", "all_subs", "variant_prediction_df", "site_map"]: continue
+                if key in [
+                    "tuned_model_params", 
+                    "all_subs", 
+                    "variant_prediction_df", 
+                    "site_map"
+                ]: continue
                 saveas += f"-{value}"
             saveas = "".join(saveas.split()) + ".png"
             fig.savefig(saveas)
@@ -140,49 +134,22 @@ def plot_pred_scatter(
 
 
 def plot_param_hist(results, show=True, save=False, printrow=False):
+
     for idx, row in results.iterrows():
-        # Make a dataframe with inferred mutational effects
-        
         if printrow: print(row)
-        
-        def split_sub(sub_string):
-            """String match the wt, site, and sub aa
-            in a given string denoting a single substitution"""
-
-            pattern = r'(?P<aawt>[A-Z\*])(?P<site>[\d\w]+)(?P<aamut>[A-Z\*])'
-            match = re.search(pattern, sub_string)
-            assert match != None, sub_string
-            return match.group('aawt'), match.group('site'), match.group('aamut')
-
-        mut_effects_dict = {
-            key : []
-            for key in ['wt', 'site', 'mut', 'param', 'param_val']
-        }
-        for (i, param) in enumerate(["β", f"S_{row.experiment_2}"]):
-            if param in row.tuned_model_params:
-                for (mutation, param_val) in zip(row.all_subs, row.tuned_model_params[param]):
-                    (wt, site, mut) = split_sub(mutation)
-                    mut_effects_dict['wt'].append(wt)
-                    mut_effects_dict['site'].append(int(site))
-                    mut_effects_dict['mut'].append(mut)
-                    mut_effects_dict['param'].append(param)
-                    mut_effects_dict['param_val'].append(float(param_val))
-
-        mut_effects_df = pd.DataFrame(mut_effects_dict)
-
-        (fig, axs) = plt.subplots(ncols=2, figsize=[10,4])
-        for (i, param) in enumerate(["β", f"S_{row.experiment_2}"]):
+        mut_effects_df = row.mutation_effects_df
+        shift_params = [f"S_{c}" for c in row.conditions[1:]]
+        fig, ax = plt.subplots(ncols=(1 + len(shift_params)), figsize=[10,4])
+        if len(shift_params) == 0: ax = [ax]
+        for (i, param) in enumerate(["β"] + shift_params):
 
             # Plot data for all mutations
-            data = mut_effects_df[
-                (mut_effects_df['mut'] != '*') &
-                (mut_effects_df['param'] == param)
-            ]
+            data = mut_effects_df[mut_effects_df['mut'] != '*']
             bin_width = 0.25
-            min_val = math.floor(data['param_val'].min()) - 0.25/2
-            max_val = math.ceil(data['param_val'].max())
+            min_val = math.floor(data[param].min()) - 0.25/2
+            max_val = math.ceil(data[param].max())
             sns.histplot(
-                x='param_val', data=data, ax=axs[i],
+                x=param, data=data, ax=ax[i],
                 stat='density', color=pal.as_hex()[0],
                 label='muts to amino acids',
                 binwidth=bin_width, binrange=(min_val, max_val),
@@ -190,30 +157,19 @@ def plot_param_hist(results, show=True, save=False, printrow=False):
             )
 
             # Plot data for mutations leading to stop codons
-            data = mut_effects_df[
-                (mut_effects_df['mut'] == '*') &
-                (mut_effects_df['param'] == param)
-            ]
+            data = mut_effects_df[mut_effects_df['mut'] == '*']
             sns.histplot(
-                x='param_val', data=data, ax=axs[i],
+                x=param, data=data, ax=ax[i],
                 stat='density', color=pal.as_hex()[1],
                 label='muts to stop codons',
                 binwidth=bin_width, binrange=(min_val, max_val),
                 alpha=0.5
-                #log_scale=True
             )
 
-            axs[i].set(xlabel=param)
+            ax[i].set(xlabel=param)
         #axs[1].set_yscale('log')
         plt.tight_layout()
-        axs[0].legend()
-        if save:
-            saveas = "param-hist"
-            for key, value in row.items():
-                if key in ["tuned_model_params", "all_subs", "variant_prediction_df", "site_map"]: continue
-                saveas += f"-{value}"
-            saveas = "".join(saveas.split()) + ".png"
-            fig.savefig(saveas)
+        ax[0].legend()
         if show: plt.show()
 
 
@@ -222,34 +178,18 @@ def plot_param_heatmap(results, show=True, save=False, printrow=False):
     for idx, row in results.iterrows():
         
         if printrow: print(row)
-        fig, ax = plt.subplots(2, figsize=(25, 10))
 
-        # TODO add outlining plots
-        # non_identical_sites = [
-        #     i for i, row in site_map.iterrows()
-        #     if row["Delta"] != row["Omicron_BA.1"]
-        # ]
+        shift_params = [f"S_{c}" for c in row.conditions[1:]]
+        total_plots = 1 + len(shift_params)
+        fig, ax = plt.subplots(total_plots, figsize=[25,(5*total_plots)])
+        if len(shift_params) == 0: ax = [ax]
+        for (i, param) in enumerate(["β"] + shift_params):
 
-        def split_sub(sub_string):
-            """String match the wt, site, and sub aa
-            in a given string denoting a single substitution"""
+            mut_effects_df = row.mutation_effects_df.copy()
+            mut_effects_df.site = mut_effects_df.site.astype(int)
 
-            pattern = r'(?P<aawt>[A-Z\*])(?P<site>[\d\w]+)(?P<aamut>[A-Z\*])'
-            match = re.search(pattern, sub_string)
-            assert match != None, sub_string
-            return match.group('aawt'), match.group('site'), match.group('aamut')
-
-        for i, param in enumerate(["β", f"S_{row.experiment_2}"]):
-            rows = []
-            for mutation, p in zip(row.all_subs, row.tuned_model_params[param]):
-                wt, site, mut = split_sub(mutation)
-                rows.append([int(site), wt, mut, float(p)])
-
-            mutation_effects = pd.DataFrame(
-                rows,
-                columns=("site", "wildtype", "mutant", param)
-            ).pivot(
-                index="mutant",
+            mutation_effects = mut_effects_df.pivot(
+                index="mut",
                 columns="site", values=param
             )
 
@@ -266,58 +206,30 @@ def plot_param_heatmap(results, show=True, save=False, printrow=False):
             ax[i].set_title(f"{param}", size=20)
 
         plt.tight_layout()
-        if save:
-            saveas = "param-heatmap"
-            for key, value in row.items():
-                if key in ["tuned_model_params", "all_subs", "variant_prediction_df", "site_map"]: continue
-                saveas += f"-{value}"
-            saveas = "".join(saveas.split()) + ".png"
-            fig.savefig(saveas)
         if show: plt.show()
 
 
-def plot_shift_by_site(results, show=True, save=False, printrow=False):
+def plot_param_by_site(results, show=True, save=False, printrow=False):
 
     for idx, row in results.iterrows():
-        
         if printrow: print(row)
-        #fig, ax = plt.subplots(2, figsize=(15, 5))
-        fig, ax = plt.subplots(1, figsize=(12, 3))
-        ax = [ax]
+        mut_effects_df = row.mutation_effects_df.copy()
+        mut_effects_df.site = mut_effects_df.site.astype(int)
 
-        non_identical_sites = [
-            int(i) for i, s in row.site_map.iterrows()
-            if s[row.experiment_ref] != s[row.experiment_2]
-        ]
-        # print(non_identical_sites)
+        shift_params = [f"S_{c}" for c in row.conditions[1:]]
+        total_plots = 1 + len(shift_params)
+        fig, ax = plt.subplots(total_plots, figsize=[12,(3*total_plots)])
+        if len(shift_params) == 0: ax = [ax]
+        for (i, param) in enumerate(["β"] + shift_params):
 
-        def split_sub(sub_string):
-            """String match the wt, site, and sub aa
-            in a given string denoting a single substitution"""
-
-            pattern = r'(?P<aawt>[A-Z\*])(?P<site>[\d\w]+)(?P<aamut>[A-Z\*])'
-            match = re.search(pattern, sub_string)
-            assert match != None, sub_string
-            return match.group('aawt'), match.group('site'), match.group('aamut')
-
-        #for i, param in enumerate(["β", f"S_{row.experiment_2}"]):
-        for i, param in enumerate([f"S_{row.experiment_2}"]):
-            rows = []
-            for mutation, p in zip(row.all_subs, row.tuned_model_params[param]):
-                wt, site, mut = split_sub(mutation)
-                rows.append([int(site), wt, mut, float(p)])
-
-            mutation_effects = pd.DataFrame(
-                rows,
-                columns=("site", "wildtype", "mutant", param)
-            ).pivot(
-                index="mutant",
+            mutation_effects = mut_effects_df.pivot(
+                index="mut",
                 columns="site", values=param
-            ).apply(lambda x: sum([abs(t) for t in x if t == t]), axis=0).reset_index()#.loc[:, 850:900]
+            ).apply(lambda x: sum([abs(t) for t in x if t == t]), axis=0).reset_index()
 
             ax[i].axhline(0, color="k", ls="--", lw=1)
-            ax[i].set_ylim([-1, 50])
-            # ax[i].set_xlim([830, 860])
+            #ax[i].set_ylim([-1, 50])
+            #ax[i].set_xlim([830, 860])
 
             sns.lineplot(
                 data=mutation_effects,
@@ -325,73 +237,103 @@ def plot_shift_by_site(results, show=True, save=False, printrow=False):
                 ax=ax[i]
             )
 
-            mutation_effects["non_identical"] = [True if s in non_identical_sites else False for s in mutation_effects.site]
+            # TODO
+            #non_identical_sites = [
+            #    int(i) for i, s in row.site_map.iterrows()
+            #    if s[row.experiment_ref] != s[row.experiment_2]
+            #]
+            #mutation_effects["non_identical"] = [
+            #    True if s in non_identical_sites else False for s in mutation_effects.site
+            #]
+
             sns.scatterplot(
                 data=mutation_effects,
-                # size=1,
                 x="site", y=0,
-                hue = "non_identical",
+                #hue = "non_identical",
                 ax=ax[i]
             )
+            # TODO more informative ylabel abs value
             ax[i].set_ylabel(f"$\sum${param}", size=10)
 
         plt.tight_layout()
-        if save:
-            saveas = "param-heatmap"
-            for key, value in row.items():
-                if key in ["tuned_model_params", "all_subs", "variant_prediction_df", "site_map"]: continue
-                saveas += f"-{value}"
-            saveas = "".join(saveas.split()) + ".png"
-            fig.savefig(saveas)
         if show: plt.show()
 
 
-def plot_fit_param_comp_scatter(results, idx_1, idx_2, show=True, save=False, printrow=False):
+def plot_fit_param_comp_scatter(
+    results,
+    fits_features,
+    show=True
+):
 
-    dfs = []
-    for fit in [idx_1, idx_2]:
-        fit_exp2 = results.loc[fit, "experiment_2"]
-        dfs.append(pd.DataFrame(
-            {
-                "all_subs" : results.loc[fit, "all_subs"],
-                 f"S_{fit_exp2}" : results.loc[fit, "tuned_model_params"][f"S_{fit_exp2}"]
-            }
-        ))
-    df = reduce(
+
+    # make sure we're comparing fits which have the same 
+    # assert results.loc[idx_1, "conditions"][0] == results.loc[idx_2, "conditions"][0] == 
+    #dfs = [row.mutation_effects_df.copy() for idx, row in results.loc[[idx_1, idx_2]].iterrows()]
+    
+    mut_effect_dfs = []
+    fit_features = []
+    for fit, feature in fits_features.items():
+        fit_mut_effects = results.loc[fit, "mutation_effects_df"].copy()
+        fit_mut_effects.site = fit_mut_effects.site.astype(int)
+        mut_effect_dfs.append(fit_mut_effects)
+        fit_features.append(feature)
+        #fit_features.append(set(
+        #    [
+        #        col for col in fit_mut_effects.columns 
+        #        if col.startswith("β") or col.startswith("S")
+        #    ]
+        #))
+        
+    #features_to_compare = set.intersection(fit_features[0], fit_features[1])
+
+    #for fit in [idx_1, idx_2]:
+    #    fit_exp2 = results.loc[fit, "experiment_2"]
+    #    dfs.append(pd.DataFrame(
+    #        {
+    #            "all_subs" : results.loc[fit, "all_subs"],
+    #             f"S_{fit_exp2}" : results.loc[fit, "tuned_model_params"][f"S_{fit_exp2}"]
+    #        }
+    #    ))
+
+    comb_mut_effects = reduce(
         lambda l, r: pd.merge(
-            l, r, how="inner", on="all_subs"
-        ), dfs
+            l, r, how="inner", on="substitution"
+        ), mut_effect_dfs
     )
-    df["is_stop"] = [True if "*" in s else False for s in df.all_subs]
-    r = pearsonr(df.iloc[:, 1], df.iloc[:, 2])[0]
-    fig, ax = plt.subplots(figsize=[6, 6])
+    comb_mut_effects["is_stop"] = [
+        True if "*" in s else False for s in comb_mut_effects.substitution
+    ]
+
+    #print(features_to_compare)
+    #print(comb_mut_effects)
+    if fit_features[0] == fit_features[1]:
+        x = f"{fit_features[0]}_x"
+        y = f"{fit_features[1]}_y"
+    else:
+        x = fit_features[0]
+        y = fit_features[1] 
+
+    fig, ax = plt.subplots(figsize=[5,4])
+    r = pearsonr(comb_mut_effects[x], comb_mut_effects[y])[0]
     sns.scatterplot(
-        data=df, 
-        x=df.columns[1], 
-        y=df.columns[2], 
+        data=comb_mut_effects, 
+        x=x, y=y,
         hue="is_stop", 
         alpha=0.6,  
         palette="deep", 
         ax=ax
     )
-    fit1_ref, fit1_e2 = results.loc[idx_1, ["experiment_ref", "experiment_2"]].values
-    fit2_ref, fit2_e2 = results.loc[idx_2, ["experiment_ref", "experiment_2"]].values
-    min1, max1 = df[f"S_{fit1_e2}"].min(), df[f"S_{fit1_e2}"].max()
+
+    #fit1_ref, fit1_e2 = results.loc[idx_1, ["experiment_ref", "experiment_2"]].values
+    #fit2_ref, fit2_e2 = results.loc[idx_2, ["experiment_ref", "experiment_2"]].values
+    min1, max1 = comb_mut_effects[x].min(), comb_mut_effects[x].max()
     ax.plot([min1, max1], [min1, max1], ls="--", c="k")
     ax.annotate(f"$r = {r:.2f}$", (.7, .1), xycoords="axes fraction", fontsize=12)
-    ax.set_title(f"Shift parameter comparison")
-    ax.set_xlabel(f"S({fit1_ref} -> {fit1_e2})")
-    ax.set_ylabel(f"S({fit2_ref} -> {fit2_e2})")
+    ax.set_title(f"feature comparison")
+    #ax.set_xlabel(f"S({fit1_ref} -> {fit1_e2})")
+    #ax.set_ylabel(f"S({fit2_ref} -> {fit2_e2})")
 
     plt.tight_layout()
-    if save:
-        saveas = "param-heatmap"
-        for key, value in row.items():
-            if key in ["tuned_model_params", "all_subs", "variant_prediction_df", "site_map"]: continue
-            saveas += f"-{value}"
-        saveas = "".join(saveas.split()) + ".png"
-        fig.savefig(saveas)
-
     if show: plt.show()
 
 
