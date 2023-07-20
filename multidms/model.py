9050ccc26fa4edc5219d4f9f4d4d3603c69d3a5d
WSDeWitt <wsdewitt@gmail.com>
2023-07-19 20:41:27 -0700

    or not...

--- multidms/model.py
+++ multidms/model.py
@@ -117,7 +117,7 @@ class Model:
 
     The mutations_df and variants_df may of course also be accessed.
 
-    >>> model.data.mutations_df  # doctest: +NORMALIZE_WHITESPACE
+    >>> model.data.mutations_df
       mutation wts  sites muts  times_seen_a  times_seen_b
     0      M1E   M      1    E             1           3.0
     1      M1W   M      1    W             1           0.0
@@ -129,29 +129,29 @@ class Model:
     features included. These are automatically updated each time you
     request the property.
 
-    >>> model.mutations_df  # doctest: +NORMALIZE_WHITESPACE, +ELLIPSIS
+    >>> model.mutations_df
       mutation wts  sites muts  times_seen_a  times_seen_b      beta  predicted_func_score  shift_b
-    0      M1E   M      1    E             1           3.0       ...                   ...      ...
-    1      M1W   M      1    W             1           0.0       ...                   ...      ...
-    2      G3P   G      3    P             1           1.0       ...                   ...      ...
-    3      G3R   G      3    R             1           2.0       ...                   ...      ...
+    0      M1E   M      1    E             1           3.0  0.080868             -2.398970      0.0
+    1      M1W   M      1    W             1           0.0 -0.386247             -2.976895      0.0
+    2      G3P   G      3    P             1           1.0 -0.375656             -2.964124      0.0
+    3      G3R   G      3    R             1           2.0  1.668974             -0.792805      0.0
 
     Notice the respective single mutation effects (``"beta"``), conditional shifts
     (``shift_d``),
     and predicted functional score (``F_d``) of each mutation in the model are now
     easily accessible. Similarly, we can take a look at the variants_df for the model,
 
-    >>> model.variants_df  # doctest: +NORMALIZE_WHITESPACE, +ELLIPSIS
+    >>> model.variants_df
       condition aa_substitutions  func_score var_wrt_ref  predicted_latent  predicted_func_score
-    0         a              M1E         2.0         M1E               ...                   ...
-    1         a              G3R        -7.0         G3R               ...                   ...
-    2         a              G3P        -0.5         G3P               ...                   ...
-    3         a              M1W         2.3         M1W               ...                   ...
-    4         b              M1E         1.0     G3P M1E               ...                   ...
-    5         b              P3R        -5.0         G3R               ...                   ...
-    6         b              P3G         0.4                           ...                   ...
-    7         b          M1E P3G         2.7         M1E               ...                   ...
-    8         b          M1E P3R        -2.7     G3R M1E               ...                   ...
+    0         a              M1E         2.0         M1E          0.080868             -2.398970
+    1         a              G3R        -7.0         G3R          1.668974             -0.792805
+    2         a              G3P        -0.5         G3P         -0.375656             -2.964124
+    3         a              M1W         2.3         M1W         -0.386247             -2.976895
+    4         b              M1E         1.0     G3P M1E         -0.294788             -2.865839
+    5         b              P3R        -5.0         G3R          1.668974             -0.792805
+    6         b              P3G         0.4                      0.000000             -2.500000
+    7         b          M1E P3G         2.7         M1E          0.080868             -2.398970
+    8         b          M1E P3R        -2.7     G3R M1E          1.749842             -0.740336
 
     We now have access to the predicted (and gamma corrected) functional scores
     as predicted by the models current parameters.
@@ -160,14 +160,14 @@ class Model:
     to the dataset. Let's take a look at the loss on the training dataset
     given our initialized parameters
 
-    >>> model.loss  # doctest: +ELLIPSIS
-    Array(..., dtype=float64)
+    >>> model.loss
+    Array(7.19312981, dtype=float64)
 
     Next, we fit the model with some chosen hyperparameters.
 
     >>> model.fit(maxiter=1000, lasso_shift=1e-5)
-    >>> model.loss  # doctest: +ELLIPSIS
-    Array(..., dtype=float64)
+    >>> model.loss
+    Array(4.37424567e-05, dtype=float64)
 
     The model tunes its parameters in place, and the subsequent call to retrieve
     the loss reflects our models loss given its updated parameters.
