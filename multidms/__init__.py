9558fe25928abb4e7e454a230ede0cb73b82a643
Jared Galloway <jaredgalloway07@gmail.com>
2023-12-06 11:36:11 -0800

    bump version 0.3.0 -> 0.3.1

--- multidms/__init__.py
+++ multidms/__init__.py
@@ -47,7 +47,7 @@ It also imports the following alphabets:
 
 __author__ = "Jared Galloway"
 __email__ = "jgallowa@fredhutch.org"
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __url__ = "https://github.com/matsengrp/multidms"
 
 from polyclonal.alphabets import AAS  # noqa: F401
