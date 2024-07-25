fc9d0fb0ef885a1bcebc11d17f4ff101ab8442ac
WSDeWitt <wsdewitt@gmail.com>
2024-07-25 13:45:31 -0700

    simplify jaxmodels

--- multidms/jaxmodels.py
+++ multidms/jaxmodels.py
@@ -3,7 +3,7 @@ r"""A simple API for global epistasis modeling."""
 import multidms
 
 import equinox as eqx
-from jaxtyping import Array, Float
+from jaxtyping import Array, Float, Int
 import jax
 import jax.numpy as jnp
 from jax.scipy.special import expit, xlogy, gammaln
@@ -23,17 +23,17 @@ class Data(eqx.Module):
         condition: The condition to extract data for.
     """
 
-    x_wt: Float[Array, "n_mutations"]
+    x_wt: Int[Array, "n_mutations"]
     """Binary encoding of the wildtype sequence."""
     pre_count_wt: Float[Array, ""]
     """Wildtype pre-selection count."""
     post_count_wt: Float[Array, ""]
     """Wildtype post-selection count."""
-    X: Float[Array, "n_variants n_mutations"]
+    X: Int[Array, "n_variants n_mutations"]
     """Variant encoding matrix (sparse format)."""
-    pre_counts: Float[Array, "n_variants"]
+    pre_counts: Int[Array, "n_variants"]
     """Pre-selection counts for each variant."""
-    post_counts: Float[Array, "n_variants"]
+    post_counts: Int[Array, "n_variants"]
     """Post-selection counts for each variant."""
     functional_scores: Float[Array, "n_variants"]
     """Functional scores for each variant."""
@@ -62,7 +62,7 @@ class Latent(eqx.Module):
 
     Args:
         data: Data to initialize the model shape for.
-        l2reg_β: L2 regularization strength for warmstart of latent models.
+        l2reg: L2 regularization strength for warmstart of latent models.
     """
 
     β0: Float[Array, ""]
@@ -73,13 +73,13 @@ class Latent(eqx.Module):
     def __init__(
         self,
         data: Data,
-        l2reg_β=0.0,
+        l2reg=0.0,
     ) -> None:
         X = scipy.sparse.csr_array(
             (data.X.data, data.X.indices.T), shape=(data.X.shape[0], len(data.x_wt))
         )
         y = data.functional_scores
-        ridge_solver = linear_model.Ridge(alpha=l2reg_β)
+        ridge_solver = linear_model.Ridge(alpha=l2reg)
         ridge_solver.fit(X, y, sample_weight=jnp.log(data.pre_counts))
         self.β0 = jnp.asarray(ridge_solver.intercept_)
         self.β = jnp.asarray(ridge_solver.coef_)
@@ -202,12 +202,10 @@ def fit(
     data_sets: dict[str, Data],
     reference_condition: str,
     l2reg_α=0.0,
-    l2reg_β=0.0,
+    l2reg=0.0,
     fusionreg=0.0,
-    maxiter=10,
-    obj_tol=1e-4,
-    block_kwargs=dict(tol=1e-8, maxiter=1000),
-) -> tuple[Model, list[float]]:
+    opt_kwargs=dict(tol=1e-8, maxiter=1000),
+) -> tuple[Model, jaxopt._src.proximal_gradient.ProxGradState]:
     r"""
     Fit a model to data.
 
@@ -215,11 +213,9 @@ def fit(
         data_sets: Data to fit to. Each key is a condition.
         reference_condition: The condition to use as a reference.
         l2reg_α: L2 regularization strength for α.
-        l2reg_β: L2 regularization strength for β.
+        l2reg: L2 regularization strength for mutation effects.
         fusionreg: Fusion (shift lasso) regularization strength.
-        maxiter: Maximum number of iterations of block coordinate descent.
-        opt_tol: Optimization tolerance for block coordinate descent.
-        block_kwargs: Keyword arguments to pass to the block solvers
+        opt_kwargs: Keyword arguments to pass to solver.
 
     Returns:
         Fitted model.
@@ -229,89 +225,55 @@ def fit(
             "WT sequence of the reference condition should have no mutations."
         )
 
-    opt_φ = jaxopt.ProximalGradient(
-        _objective_φ_preconditioned,
-        prox=_prox_φ,
+    opt = jaxopt.ProximalGradient(
+        _objective_smooth_preconditioned,
+        prox=_prox,
         value_and_grad=True,
-        **block_kwargs,
+        **opt_kwargs,
     )
-    opt_α = jaxopt.ProjectedGradient(
-        _objective_α,
-        _proj_α,
-        value_and_grad=True,
-        **block_kwargs,
-    )
-
-    Ps = {d: jnp.diag(1 / (1 + data_sets[d].X.sum(0).todense())) for d in data_sets}
 
     model = Model(
-        φ={d: Latent(data_sets[d], l2reg_β=l2reg_β) for d in data_sets},
+        φ={d: Latent(data_sets[d], l2reg=l2reg) for d in data_sets},
         α={d: jnp.ptp(data_sets[d].functional_scores) for d in data_sets},
         reference_condition=reference_condition,
     )
 
-    kwargs = dict(l2reg_α=l2reg_α, l2reg_β=l2reg_β)
+    Ps = {d: jnp.diag(1 / (1 + data_sets[d].X.sum(axis=0).todense())) for d in data_sets}
 
-    filter_spec = Model(φ=True, α=False, reference_condition=reference_condition)
+    hyperparameters = dict(fusionreg=fusionreg, Ps=Ps)
+    args = (data_sets, Ps)
+    kwargs = dict(l2reg_α=l2reg_α, l2reg=l2reg)
 
-    obj_traj = [_objective(model, data_sets, fusionreg=fusionreg, **kwargs)]
+    model, state = opt.run(model, hyperparameters, *args, **kwargs)
 
-    for iter in range(maxiter):
-        try:
-            print(f"iteration {iter + 1}")
-
-            model_φ, model_α = eqx.partition(model, filter_spec)
-            model_α, state_α = opt_α.run(model_α, None, model_φ, data_sets, **kwargs)
-            model = eqx.combine(model_φ, model_α)
-            obj_traj.append(_objective(model, data_sets, fusionreg=fusionreg, **kwargs))
-            print(f"  α block:")
-            print(f"    iterations={state_α.iter_num}")
-            print(f"    error={state_α.error:.2e}")
-            print(f"    α={jax.tree_map(lambda x: round(float(x), 2), model.α)}")
-
-            model_φ, model_α = eqx.partition(model, filter_spec)
-            model_φ, state_φ = opt_φ.run(
-                model_φ,
-                dict(fusionreg=fusionreg, Ps=Ps),
-                model_α,
-                data_sets,
-                Ps,
-                **kwargs,
-            )
-            model = eqx.combine(model_φ, model_α)
-            obj_traj.append(
-                _objective(model, data_sets, fusionreg=fusionreg, **kwargs)
-            )
-            print(f"  φ block:")
-            print(f"    iterations={state_φ.iter_num}")
-            print(f"    error={state_φ.error:.2e}")
-            for d in data_sets:
-                if d != reference_condition:
-                    shifts = model.φ[d].β - model.φ[reference_condition].β
-                    print(
-                        f"    {d} shift sparsity={(shifts == 0).sum() / len(shifts):.2%}"
-                    )
-
-            Δobj = (obj_traj[-3] - obj_traj[-1]) / max(
-                jnp.abs(obj_traj[-3]), jnp.abs(obj_traj[-1]), 1
-            )
-            print(f"  {Δobj=:.2e}")
-
-            if Δobj < obj_tol:
-                break
-        except KeyboardInterrupt:
-            print(f"Interrupted at iteration {iter + 1}")
-            break
-
-    return model, obj_traj
+    return model, state
 
 
 # The following private functions are used internally by the fit function
 
 
-def _objective(model, data_sets, fusionreg=0.0, l2reg_α=0.0, l2reg_β=0.0):
+@jax.jit
+@jax.value_and_grad
+def _objective_smooth(model, data_sets, l2reg_α=0.0, l2reg=0.0):
+    n = sum(data_set.X.shape[0] for data_set in data_sets.values())
+    loss = sum(model.loss(data_sets).values())
+    ridge_α = sum((model.α[d] ** 2).sum() for d in model.φ)
+    ridge_β = sum((model.φ[d].β ** 2).sum() for d in model.φ)
+    return loss / n + l2reg_α * ridge_α + l2reg * ridge_β
+
+
+@jax.jit
+def _objective_smooth_preconditioned(model, data_sets, Ps, l2reg_α=0.0, l2reg=0.0):
+    value, grad = _objective_smooth(model, data_sets, l2reg_α, l2reg)
+    for d in model.φ:
+        P = Ps[d]
+        grad = eqx.tree_at(lambda grad_: grad_.φ[d].β, grad, P @ grad.φ[d].β)
+    return value, grad
+
+
+def _objective(model, data_sets, fusionreg=0.0, l2reg_α=0.0, l2reg=0.0):
     return _objective_smooth(
-        model, data_sets, l2reg_α=l2reg_α, l2reg_β=l2reg_β
+        model, data_sets, l2reg_α=l2reg_α, l2reg=l2reg
     ) + fusionreg * sum(
         jnp.abs(model.φ[d].β - model.φ[model.reference_condition].β).sum()
         for d in model.φ
@@ -320,65 +282,20 @@ def _objective(model, data_sets, fusionreg=0.0, l2reg_α=0.0, l2reg_β=0.0):
 
 
 @jax.jit
-def _objective_smooth(model, data_sets, l2reg_α=0.0, l2reg_β=0.0):
-    n = sum(data_set.X.shape[0] for data_set in data_sets.values())
-    loss = sum(model.loss(data_sets).values())
-    ridge_α = sum((model.α[d] ** 2).sum() for d in model.φ)
-    # NOTE: ridge wrt mean
-    ridge_β = sum(((model.φ[d].β - model.φ[d].β.mean()) ** 2).sum() for d in model.φ)
-    return loss / n + l2reg_α * ridge_α + l2reg_β * ridge_β
-
-
-@jax.jit
-@jax.value_and_grad
-def _objective_φ(model_φ, model_α, data_sets, l2reg_α=0.0, l2reg_β=0.0):
-    model = eqx.combine(model_φ, model_α)
-    return _objective_smooth(model, data_sets, l2reg_α=l2reg_α, l2reg_β=l2reg_β)
-
-
-@jax.jit
-def _objective_φ_preconditioned(
-    model_φ, model_α, data_sets, Ps, l2reg_α=0.0, l2reg_β=0.0
-):
-    value, grad = _objective_φ(
-        model_φ, model_α, data_sets, l2reg_α=l2reg_α, l2reg_β=l2reg_β
-    )
-    for d in data_sets:
-        gradβ = grad.φ[d].β
-        # apply preconditioner
-        P = Ps[d]
-        gradβ = P @ gradβ
-        grad = eqx.tree_at(lambda model_: model_.φ[d].β, grad, gradβ)
-    return value, grad
-
-
-@jax.jit
-@jax.value_and_grad
-def _objective_α(model_α, model_φ, data_sets, l2reg_α=0.0, l2reg_β=0.0):
-    model = eqx.combine(model_α, model_φ)
-    return _objective_smooth(model, data_sets, l2reg_α=l2reg_α, l2reg_β=l2reg_β)
-
-
-@jax.jit
-def _prox_φ(model, hyperparameters, scaling=1.0):
-    # NOTE: for preconditioned proximal gradient, we need to use the preconditioning matrix P in the prox
+def _prox(model, hyperparameters, scaling=1.0):
     fusionreg = hyperparameters["fusionreg"]
     Ps = hyperparameters["Ps"]
+    # fused lasso for β
     β_ref = model.φ[model.reference_condition].β
     for d in model.φ:
         if d != model.reference_condition:
+            β0 = model.φ[d].β0
             β = model.φ[d].β
             P = Ps[d]
-            β_prox = β_ref + jaxopt.prox.prox_lasso(
-                β - β_ref, fusionreg * jnp.diag(P), scaling
-            )
+            β_prox = β_ref + jaxopt.prox.prox_lasso(β - β_ref, fusionreg * jnp.diag(P), scaling)
+            model = eqx.tree_at(lambda model_: model_.φ[d].β0, model, β0)
             model = eqx.tree_at(lambda model_: model_.φ[d].β, model, β_prox)
-    return model
-
-
-@jax.jit
-def _proj_α(model, hyperparameters=None):
-    # box constrain alpha to the non-negative orthant
+    # box constrain α to the non-negative orthant
     for d in model.φ:
         model = eqx.tree_at(
             lambda model: model.α[d], model, jnp.clip(model.α[d], 0.0, jnp.inf)
